c14933b36c72982ba3298c667a19e3e5247bc708
AmorosJulian <amoros.julian@hotmail.com>
2021-12-21 13:44:00 -0300

    actualizo a desafio dos

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+alert("Hola! este es un desafio que solo mi tutora de Js puede ver..");
+let nombre = prompt("Para corroborarlo, por favor ingresa tu nombre:");
+let edad = parseInt(prompt("Tambien necesito verificar tu edad:"));
+
+if (nombre.toLowerCase() == "karen" && edad >= 18) {
+    alert("Bienvenida " + nombre + ", este es mi desafio 2 de Js !")
+} else if(nombre.toLowerCase() !== "karen" ||  edad < 18 ) {
+    alert("No estas autorizado para ingresar a mi desafio...")
+} else {
+    alert("Lo siento, sólo mi tutora puede ingresar...")
+}
+     
