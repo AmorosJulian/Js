3e9781834c574c67b6de48a22c0ff4b2f698dcb3
AmorosJulian <amoros.julian@hotmail.com>
2022-01-12 23:38:58 -0300

    comienzo a estructurar los prompt para poder elegir autos - preentrega 1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
- class autos {
-=======
 class autos {
->>>>>>> c55c7424b5c8de08be25a7430335fded58b48cbb
+
     constructor (marca, modelo, año, precio) {
         this.marca = marca.toUpperCase();
         this.modelo = modelo.toUpperCase();
@@ -26,30 +23,66 @@ const auto3 = new autos ("Chevrolet" , "Corsa" , 2014 , 5500);
 const auto4 = new autos ("Volkswagen" , "Fox" , 2020 , 7000);
 const auto5 = new autos ("Fiat" , "Cronos" , 2021 , 8400);
 
-//Muestro un auto, luego le aplico precio con tarjeta, lo alquilo y vuelvo a mostrar el estado del auto
-// console.log(auto1);
-// auto1.precioTarjeta();
-// auto1.alquilar();
-// console.log(auto1);
-// console.log(auto4);
-// auto4.precioTarjeta();
-// auto4.alquilar();
-// console.log(auto4);
+alert(`Bienvenido a RENT-A-CAR`);
+let choise = prompt(`Elija uno de nuestros autos disponibles:  \n 1- Nissan Versa 2015 \n 2- Chevrolet Onix 2018 \n 3- Chevrolet Corsa 2014 \n 4- Volkswagen Fox 2020 \n 5- Fiat Cronos 2021 `);
 
-// Defino un Array vacio para luego pushear los productos
-const arrAutos = [];
-// Agrego objetos mediante push
-arrAutos.push(auto1, auto2, auto3, auto4, auto5, new autos ("Toyota".toUpperCase() , "Etios".toUpperCase() , 2019 , 8100));
-console.log(arrAutos);
-// Iteracion para recorrer todos los objetos y extraer informacion detallada
-console.log("En el array existen las siguientes Marcas:")
-for (const marca of arrAutos) {
-    console.log("Marca: " + marca.marca);
+switch(choise) {
+    case "1":
+        let choiseCar1 = prompt(`Nissa Versa 2015. \n Desea ver precio en : \n 1- EFECTIVO \n 2- TARJETA`)
+        if((choiseCar1 == 1) || (choiseCar1 == "efectivo")) {
+            alert(`El precio en efectivo es ${auto1.precio}`)
+            
+        }
+        else if ((choiseCar1 == 2) || (choiseCar1 == "tarjeta")) {
+            auto1.precioTarjeta();
+            alert(`El precio con tarjeta es ${auto1.precio}`)
+        } 
+        break;
+    case "2":
+        let choiseCar2 = prompt(`Chevrolet Onix 2018. \n Desea ver precio en : \n 1- EFECTIVO \n 2- TARJETA`)
+        if((choiseCar2 == 1) || (choiseCar2 == "efectivo")) {
+            alert(`El precio en efectivo es ${auto2.precio}`)
+                
+        }
+        else if ((choiseCar2 == 2) || (choiseCar2 == "tarjeta")) {
+            auto2.precioTarjeta();
+            alert(`El precio con tarjeta es ${auto2.precio}`)
+        } 
+        break;  
+        case "3":
+            let choiseCar3 = prompt(`Chevrolet Corsa 2014. \n Desea ver precio en : \n 1- EFECTIVO \n 2- TARJETA`)
+            if((choiseCar3 == 1) || (choiseCar3 == "efectivo")) {
+                alert(`El precio en efectivo es ${auto3.precio}`)
+                    
+            }
+            else if ((choiseCar3 == 2) || (choiseCar3 == "tarjeta")) {
+                auto3.precioTarjeta();
+                alert(`El precio con tarjeta es ${auto3.precio}`)
+            } 
+            break; 
+            case "4":
+            let choiseCar4 = prompt(`Volkswagen Fox 2020. \n Desea ver precio en : \n 1- EFECTIVO \n 2- TARJETA`)
+            if((choiseCar4 == 1) || (choiseCar4 == "efectivo")) {
+                alert(`El precio en efectivo es ${auto4.precio}`)
+                    
+            }
+            else if ((choiseCar4 == 2) || (choiseCar4 == "tarjeta")) {
+                auto4.precioTarjeta();
+                alert(`El precio con tarjeta es ${auto4.precio}`)
+            } 
+            break;  
+            case "5":
+                let choiseCar5 = prompt(`Fiat Cronos 2021. \n Desea ver precio en : \n 1- EFECTIVO \n 2- TARJETA`)
+                if((choiseCar5 == 1) || (choiseCar5 == "efectivo")) {
+                    alert(`El precio en efectivo es ${auto5.precio}`)
+                        
+                }
+                else if ((choiseCar5 == 2) || (choiseCar5 == "tarjeta")) {
+                    auto5.precioTarjeta();
+                    alert(`El precio con tarjeta es ${auto5.precio}`)
+                } 
+                break;           
 }
-const menorPrecio = arrAutos.filter( precio => precio.precio <= 7000);
-console.log("Con un presupuesto de 7000 podes alquilar estos vehiculos: ")
-console.log(menorPrecio);
-
 
 
 
